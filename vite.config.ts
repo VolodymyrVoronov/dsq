9b65356ac092555bdc71da6707d48e2d07a31619
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-06-13 20:01:50 +0200

    chore(global): ffxied linter errors.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
@@ -8,9 +7,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
 });
